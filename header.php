af8ba89c142c5efddcee53b27007bf8c9e9195dc
featherplain <info@featherplain.com>
2015-12-07 21:57:17 +0900

    make header and footer #21

--- header.php
+++ header.php
@@ -21,30 +21,38 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'asknode1' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'asknode1' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+<div class="l-container">
+  <header role="banner" class="l-header">
+  	<div class="siteBrand">
+	    <h1 class="siteBrand__title">
+	    	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a>
+	    </h1>
+	    <?php // site description
+  		$description = get_bloginfo( 'description', 'display' );
+  		echo '<p class="siteBrand__description">' . $description /* WPCS: xss ok. */ . '</p>' ;
+	     ?>
+  	</div>
+    <a href="#" class="btnMenuTrigger"></a>
+    <nav id="gNav" class="gNav">
+      <ul class="gMenu">
+        <li class="gMenu__item"><a href="./about.html">ABOUT</a></li>
+        <li class="gMenu__item gMenu__item--has-child"><a href="./archive-service.html">SERVICE</a>
+          <ul class="subMenu js-dropdown">
+            <li><a href="./single-service.html">DESIGN</a></li>
+            <li><a href="./service/coding/">CODING</a></li>
+            <li><a href="./service/wordpress/">WordPress</a></li>
+            <li><a href="./service/writing/">WRITING</a></li>
+          </ul>
+        </li>
+        <li class="gMenu__item"><a href="./works-archive.html">WORKS</a></li>
+        <li class="gMenu__item"><a href="./contact.html">CONTACT</a></li>
+        <li class="gMenu__item"><a href="./post-archive.html">BLOG</a></li>
+        <li class="gMenu__item">
+          <ul class="socialMenu">
+            <li class="socialMenu__item"><a href="https://facebook.com/megumi.hano/" target="_blank" title="Facebook"><span class="readerOnly">Facebook</span></a></li>
+            <li class="socialMenu__item"><a href="https://github.com/featherplain/" target="_blank" title="GitHub"><span class="readerOnly">GitHub</span></a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav><!-- / .gNav -->
+  </header><!-- / .l-header -->
