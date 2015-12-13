ad2b69a8c533c0ce6182331af379ad40f0a9caa7
featherplain <info@featherplain.com>
2015-12-14 00:27:15 +0900

    make sns share buttons #35

--- src/js/app/sns.js
+++ src/js/app/sns.js
@@ -1,30 +1,30 @@
 (function($){
   $(document).ready(function(){
     // facebook
-    // var facebook = {
-    //   init: function() {
+    var facebook = {
+      init: function() {
 
-    //     this.open();
+        this.open();
 
-    //     var url = 'http://graph.facebook.com/?id=' + encodeURIComponent(location.href);
-    //     $.ajax({
-    //       url: url,
-    //       dataType: 'jsonp',
-    //       success: function(json) {
-    //         var count = json.shares ? json.shares : 0;
-    //         $('#main').find('.count--fb').html(count);
-    //       }
-    //     });
-    //   },
-    //   open: function() {
-    //     var $target = $('#main').find('.btnShare--fb');
-    //     $target.on('click', function(event) {
-    //       event.preventDefault();
-    //       window.open($(this).attr('href'), 'facebook', 'width=670, height=400, menubar=no, toolbar=no, scrollbars=yes');
-    //     });
-    //   }
-    // }
-    // facebook.init();
+        var url = 'http://graph.facebook.com/?id=' + encodeURIComponent(location.href);
+        $.ajax({
+          url: url,
+          dataType: 'jsonp',
+          success: function(json) {
+            var count = json.shares ? json.shares : 0;
+            $('#js-sns').find('.js-count--fb').html(count);
+          }
+        });
+      },
+      open: function() {
+        var $target = $('#js-sns').find('.js-share-fb');
+        $target.on('click', function(event) {
+          event.preventDefault();
+          window.open($(this).attr('href'), 'facebook', 'width=670, height=400, menubar=no, toolbar=no, scrollbars=yes');
+        });
+      }
+    }
+    facebook.init();
     // Facebook Like Button
     (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
@@ -34,53 +34,53 @@
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
     // Hatebu
-    // var hatebu = {
-    //   init: function() {
+    var hatebu = {
+      init: function() {
 
-    //     this.open();
+        this.open();
 
-    //     var url = 'http://api.b.st-hatena.com/entry.count?url=' + encodeURIComponent(location.href);
-    //     $.ajax({
-    //       url: url,
-    //       dataType: 'jsonp',
-    //       success: function(json) {
-    //         var count = json ? json : 0;
-    //         $('#main').find('.count--ha').html(count);
+        var url = 'http://api.b.st-hatena.com/entry.count?url=' + encodeURIComponent(location.href);
+        $.ajax({
+          url: url,
+          dataType: 'jsonp',
+          success: function(json) {
+            var count = json ? json : 0;
+            $('#js-sns').find('.js-count--ha').html(count);
 
-    //         if(typeof(count) == 'undefined'){
-    //           count = 0;
-    //         }
-    //       }
-    //     });
-    //   },
-    //   open: function() {
-    //     var $target = $('#main').find('.btnShare--ha');
-    //     $target.on('click', function(event) {
-    //       event.preventDefault();
-    //       window.open($(this).attr('href'), 'はてなブックマークブックマークレット', 'width=550, height=420, menubar=no, toolbar=no, scrollbars=yes');
-    //     });
-    //   }
-    // }
-    // hatebu.init();
-    // // Twitter
-    // var twitter = {
-    //   open: function() {
-    //     var $target = $('#main').find('.btnShare--tw');
-    //     $target.on('click', function(event) {
-    //       event.preventDefault();
-    //       window.open($(this).attr('href'), 'Twitter でリンクを共有する', 'width=550, height=400, menubar=no, toolbar=no, scrollbars=yes');
-    //     });
-    //   }
-    // }
-    // twitter.init();
-    // // pocket
-    // !function(d,i){
-    //   if(!d.getElementById(i)){
-    //     var j=d.createElement("script");
-    //     j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";
-    //     var w=d.getElementById(i);
-    //     d.body.appendChild(j);}
-    // }
-    // (document,"pocket-btn-js");
+            if(typeof(count) == 'undefined'){
+              count = 0;
+            }
+          }
+        });
+      },
+      open: function() {
+        var $target = $('#js-sns').find('.js-share-ha');
+        $target.on('click', function(event) {
+          event.preventDefault();
+          window.open($(this).attr('href'), 'はてなブックマークブックマークレット', 'width=550, height=420, menubar=no, toolbar=no, scrollbars=yes');
+        });
+      }
+    }
+    hatebu.init();
+    // Twitter
+    var twitter = {
+      open: function() {
+        var $target = $('#js-sns').find('.js-share-tw');
+        $target.on('click', function(event) {
+          event.preventDefault();
+          window.open($(this).attr('href'), 'Twitter でリンクを共有する', 'width=550, height=400, menubar=no, toolbar=no, scrollbars=yes');
+        });
+      }
+    }
+    twitter.open();
+    // pocket
+    !function(d,i){
+      if(!d.getElementById(i)){
+        var j=d.createElement("script");
+        j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";
+        var w=d.getElementById(i);
+        d.body.appendChild(j);}
+    }
+    (document,"pocket-btn-js");
   });
 })(jQuery);
--- template-parts/content-sns.php
+++ template-parts/content-sns.php
@@ -10,7 +10,7 @@
 $url = get_template_directory_uri();
 ?>
 
-<div class="snsShare">
+<div id="js-sns" class="snsShare">
 
 	<?php // set attachment image
 	if ( has_post_thumbnail() ) {
@@ -19,34 +19,33 @@ $url = get_template_directory_uri();
 		echo '<div style="background-image: url(' . $url . '/assets/images/img_noimage.png);" class="fbLike">';
 	}
   ?>
-
 		<div class="fbLike__overlay">
 			<?php the_title( '<h3 class="fbLike__title">', '</h3>' ); ?>
 			<div class="fbLike__btnWrap">
 				<div class="fb-like"></div>
 			</div>
 		</div>
-	</div>
+	</div><!-- / .fbLike -->
 	<ul class="snsShare__list">
 		<li class="snsShare__item">
-			<a href="#" class="btnShare btnShare--fb">
+			<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>" class="btnShare btnShare--fb js-share-fb">
 				<svg role="img" width="18px" height="18px" class="btnShare__icon">
 					<use xlink:href="<?php echo $url;?>/assets/images/sprite.symbol.svg#icon_fb"></use>
 				</svg>
 				<span class="btnShare__text">シェア</span>
-				<span class="js-count js-count--fb">1000</span>
+				<span class="js-count js-count--fb"></span>
 			</a>
 		</li>
 		<li class="snsShare__item">
-			<a href="#" class="btnShare btnShare--ha">
+			<a href="http://b.hatena.ne.jp/add?mode=confirm&amp;url=<?php echo urlencode( get_permalink() ); ?>&amp;title=<?php echo urlencode(get_the_title() ); ?>" class="btnShare btnShare--ha js-share-ha">
 				<svg role="img" width="18px" height="16px" class="btnShare__icon">
 					<use xlink:href="<?php echo $url;?>/assets/images/sprite.symbol.svg#icon_ha"></use>
 				</svg><span class="btnShare__text">はてブ</span>
-				<span class="js-count js-count--ha">1000</span>
+				<span class="js-count js-count--ha"></span>
 			</a>
 		</li>
 		<li class="snsShare__item">
-			<a href="#" class="btnShare btnShare--tw">
+			<a href="https://twitter.com/intent/tweet?via=featherplain&amp;url=<?php echo urlencode( get_permalink() ); ?>&amp;text=<?php echo urlencode( get_the_title() ); ?>" class="btnShare btnShare--tw js-share-tw">
 				<svg role="img" width="20px" height="16px" class="btnShare__icon">
 					<use xlink:href="<?php echo $url;?>/assets/images/sprite.symbol.svg#icon_tw"></use>
 				</svg>
@@ -54,11 +53,9 @@ $url = get_template_directory_uri();
 			</a>
 		</li>
 		<li class="snsShare__item">
-			<a href="#" class="btnShare btnShare--po">
-				<svg role="img" width="18px" height="16px" class="btnShare__icon">
-					<use xlink:href="<?php echo $url;?>/assets/images/sprite.symbol.svg#icon_po"></use>
-				</svg><span class="btnShare__text">後で読む</span>
-			</a>
+			<div class="btnShare btnShare--po">
+				<a href="https://getpocket.com/save" data-lang="ja" data-pocket-count="none" data-pocket-label="pocket" class="pocket-btn"></a>
+			</div>
 		</li>
-	</ul>
+	</ul><!-- / .snsShare__list -->
 </div><!-- / .snsShare -->
