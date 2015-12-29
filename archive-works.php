d445ea244a52c41e06d1bf2ccd95b6fb9ee62c0b
featherplain <info@featherplain.com>
2015-12-29 14:25:58 +0900

    switch to toe columns layout on  blog archive 57

---
+++ archive-works.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying archive works pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package asknode
+ */
+
+get_header(); ?>
+
+	<div class="l-main">
+		<h2 class="pageTitle">WORKS</h2>
+		<p class="archiveTitle">
+			<span class="archiveTitle__inner"><?php custom_archive_title(); ?></span>
+		</p>
+
+		<div class="row">
+			<div class="postArchive">
+				<?php
+				if ( have_posts() ) :
+
+					while ( have_posts() ) : the_post(); // Start the Loop
+
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
+
+					endwhile; // End the Loop
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif; ?>
+			</div><!-- / .postArchive -->
+		</div><!-- / .row -->
+
+		<?php
+		the_posts_navigation(array(
+		    'prev_text' => '前へ',
+		    'next_text' => '次へ'
+		)); ?>
+
+	</div><!-- / .l-main -->
+<?php
+get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -8,24 +8,36 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" class="postArchive__article">
-	<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark" class="postArchive__link">
-    <div class="postArchive__thumb">
+<?php
+// Category label
+$cats = get_the_category();
+$catHtml = '';
+foreach ( $cats as $cat) {
+	$catHtml .= '<a href="' . get_category_link( $cat->cat_ID ) . '">' . $cat->name . '</a>';
+}
+?>
 
-			<?php // post thumbnail
-
-			if ( has_post_thumbnail() ) {
-				the_post_thumbnail( 'thumb308x180' );
-			}
-			else {
-				echo '<img src="' . get_template_directory_uri() . '/assets/images/thumb_noimage.png" />';
-			}
-			?>
-
-    </div>
-    <div class="postArchive__content">
-      <h3 class="postArchive__title"><?php $title = mb_substr( $post->post_title, 0, 56); echo $title ;?></h3>
-      <time class="postArchive__date"><?php the_time( get_option( 'date_format' ) ); ?></time>
-    </div>
-	</a>
-</article>
+<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+	<article id="post-<?php the_ID(); ?>" class="post post--archive">
+	  <header class="post__header">
+	    <div class="post__meta">
+	      <time><?php the_time( get_option( 'date_format' ) ); ?></time>
+	      <span><?php echo $catHtml; ?></span>
+	    </div>
+	    <?php the_title( '<h1 class="post__title">', '</h1>' ) ?>
+	    <div class="post__excerpt">
+	      <?php the_excerpt(); ?>
+	    </div>
+		  <i class="post__separator">
+		    <svg role="img" width="38px" height="6px">
+		      <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.symbol.svg#icon_dots"></use>
+		    </svg>
+		  </i>
+	  </header>
+		<?php if ( has_post_thumbnail() ): ?>
+			<div class="post__image">
+				<?php the_post_thumbnail( 'thumb870x504' ); ?>
+			</div>
+		<?php endif ; ?>
+	</article><!-- / .post -->
+</a>
