99f1cf24849f524a34e5601f851d9aeb7a4767ca
featherplain <info@featherplain.com>
2016-01-01 20:44:34 +0900

    add more button #57

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,27 +17,21 @@ foreach ( $cats as $cat) {
 }
 ?>
 
-<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
 	<article id="post-<?php the_ID(); ?>" class="post post--archive">
-	  <header class="post__header">
-	    <div class="post__meta">
-	      <time><?php the_time( get_option( 'date_format' ) ); ?></time>
-	      <span><?php echo $catHtml; ?></span>
-	    </div>
-	    <?php the_title( '<h1 class="post__title">', '</h1>' ) ?>
-	    <div class="post__excerpt">
-	      <?php the_excerpt(); ?>
-	    </div>
-		  <i class="post__separator">
-		    <svg role="img" width="38px" height="6px">
-		      <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.symbol.svg#icon_dots"></use>
-		    </svg>
-		  </i>
-	  </header>
+    <div class="post__meta">
+      <time><?php the_time( get_option( 'date_format' ) ); ?></time>
+      <span><?php echo $catHtml; ?></span>
+    </div>
+  	<h1 class="post__title">
+  		<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a>
+  	</h1>
+    <div class="post__excerpt">
+      <?php the_excerpt(); ?>
+    </div>
 		<?php if ( has_post_thumbnail() ): ?>
 			<div class="post__image">
 				<?php the_post_thumbnail( 'thumb870x504' ); ?>
 			</div>
 		<?php endif ; ?>
+		<a href="<?php echo esc_url( get_permalink() ); ?>" class="btnGhost btnGhost--more">続きを読む</a>
 	</article><!-- / .post -->
-</a>
