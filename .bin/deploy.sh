858d6a3f0b5fdcdc5cfa174d9fda2ae3f453a9f8
featherplain <info@featherplain.com>
2016-02-06 17:40:07 +0900

    modify deaploy.sh

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -30,7 +30,7 @@ bower_components
 .DS_store" > .gitignore
 
 eval "$(ssh-agent -s)" # start the ssh agent
-- chmod 600 deploy_key
-- mv deploy_key ~/.ssh/id_rsa
-- git remote add deploy ${DEPLOY_PATH}
-- git push deploy
+chmod 600 deploy_key
+mv deploy_key ~/.ssh/id_rsa
+git remote add deploy ${DEPLOY_PATH}
+git push deploy
