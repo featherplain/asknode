2bd15f01c81c54f087690ed2ddade64658c97931
featherplain <info@featherplain.com>
2016-02-06 16:54:53 +0900

    tr build.sh && modufy deploy.sh

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+  echo "Not deploying pull requests."
+  exit
+fi
+
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+  echo "Not on the 'master' branch."
+  exit
+fi
+
+rm -rf .git
+rm -r .gitignore
+
+echo ".editorconfig
+.travis.yml
+README.md
+.bin
+bower.json
+gulpfile.js
+node_modules
+package.json
+src
+README.md
+setting.json
+bower_components
+.DS_store" > .gitignore
+
 eval "$(ssh-agent -s)" # start the ssh agent
 chmod 600 .travis/deploy_key.pem # this key should have push access
 ssh-add .travis/deploy_key.pem
